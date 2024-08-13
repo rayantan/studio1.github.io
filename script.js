876a00e395682472b02a981482684547344c1577
rayantan <rayantan2003@gmail.com>
2024-08-13 09:42:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change navbar style on scroll
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
+
